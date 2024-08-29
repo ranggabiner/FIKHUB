6b57e3b22bf086441f9b095f980373243e0849dc
ranggaupnvj <2310501014@mahasiswa.upnvj.ac.id>
2024-08-29 21:48:57 +0700

    programs copywrite

--- FIKHUB/Views/ProgramsView.swift
+++ FIKHUB/Views/ProgramsView.swift
@@ -1,5 +1,5 @@
 //
-//  ProdiView.swift
+//  ProgramsView.swift
 //  FIKHUB
 //
 //  Created by Rangga Biner on 29/08/24.
@@ -7,8 +7,8 @@
 
 import SwiftUI
 
-struct ProdiView: View {
-    let semesters: [Int:String] = [
+struct ProgramsView: View {
+    let programs: [Int:String] = [
         1: "D3 Sistem Informasi",
         2: "S1 Sistem Informasi",
         3: "S1 Informatika",
@@ -16,7 +16,7 @@ struct ProdiView: View {
 
     var body: some View {
         Form {
-            ForEach(Array(semesters.sorted(by: { $0.key < $1.key })), id: \.key) { key, value in
+            ForEach(Array(programs.sorted(by: { $0.key < $1.key })), id: \.key) { key, value in
                 Button(action: {
                     print("tap \(value)")
                 }) {
@@ -29,5 +29,5 @@ struct ProdiView: View {
 }
 
 #Preview {
-    ProdiView()
+    ProgramsView()
 }
